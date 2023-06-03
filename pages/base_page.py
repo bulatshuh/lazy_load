3030ce3cdf53ba3f8fa9cc2af679e58f28ebd74e
bulatshuh <bulat.khisamutdinov.00@mail.ru>
2023-06-03 10:35:26 +0400

    add locators file

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,3 +1,7 @@
+import time
+from .locators import BasePageLocators
+
+
 class BasePage:
     def __init__(self, browser, url):
         self.browser = browser
@@ -8,10 +12,24 @@ class BasePage:
         self.browser.implicitly_wait(5)
 
     def take_screenshot(self):
-        self.browser.get_screenshot_as_file('/tmp/google.png')
+        element = self.browser.find_element(*BasePageLocators.BODY)
+        element.screenshot('C://users/Bulatshuh/lazy_load/temp/yahoo.png')
 
     def scroll_down_pixels(self, pixels):
         self.browser.execute_script(f'window.scrollBy(0, {pixels});')
+
+    def scroll_down_absolute(self):
+        height = self.browser.execute_script("return document.body.scrollHeight")
+        while int(height) > 1500:
+            self.browser.execute_script(f'window.scrollBy(0, 1500);')
+            time.sleep(2)
+            height -= 1500
+        else:
+            self.browser.execute_script(f'window.scrollBy(0, 1500);')
+            time.sleep(2)
+
+    def go_full_screen(self):
+        self.browser.maximize_window()
 
     def close(self):
         self.browser.quit()
---
+++ pages/locators.py
@@ -0,0 +1,5 @@
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+    BODY = (By.CSS_SELECTOR, '.fp-body.display-push-promos')
